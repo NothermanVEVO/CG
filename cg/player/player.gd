c9c053a11f8eebb44beda221bed76b2fa292a6c1
Notherman <134525115+NothermanVEVO@users.noreply.github.com>
2025-10-24 14:14:25 -0300

    2025/10/24 - 001
    - Removed unnecessary code.
    - Added door and collision to it.
    - Organized the blender items.
    - Added a ball with physics and collision.

--- cg/player/player.gd
+++ cg/player/player.gd
@@ -19,27 +19,16 @@ func _physics_process(delta: float) -> void:
 	direction = Input.get_vector("Move Left", "Move Right", "Move Front", "Move Back")
 	
 	if direction:
-		velocity.x = direction.x * SPEED * delta ## LEFT OR RIGHT
-		velocity.z = direction.y * SPEED * delta ## BACK OR FRONT
+		var direction3 := Vector3(direction.x, 0, direction.y).rotated(Vector3(0, 1, 0), $PerspectiveCamera3.rotation.y)
+		velocity.x = direction3.x * SPEED * delta ## LEFT OR RIGHT
+		velocity.z = direction3.z * SPEED * delta ## BACK OR FRONT
 	else:
 		velocity.x *= DECELERATION
 		velocity.z *= DECELERATION
 	
-	velocity = velocity.rotated(Vector3(0, 1, 0), $PerspectiveCamera3.rotation.y)
-	#print($PerspectiveCamera3.rotation.y)
+	#velocity = velocity.rotated(Vector3(0, 1, 0), $PerspectiveCamera3.rotation.y)
 	
 	if direction:
 		_corpo.rotation.y = lerp_angle(_corpo.rotation.y, Vector2(velocity.z, velocity.x).angle(), delta * 16)
 	
 	move_and_slide()
-
-func fuck_u_angle(current_angle : float, new_angle : float) -> void:
-	if new_angle < 0:
-			new_angle = PI * 2 + new_angle
-	
-	#current_angle += new_angle
-	
-	if current_angle >= PI * 2:
-		current_angle = fmod(PI * 2, current_angle)
-	
-	print("Current Angle: " + str(current_angle))
